7dd6874846f0112f1f519ba5b005b6cb8b073b2c
snamilah <ctnabihahamila98@gmail.com>
2020-10-27 07:53:22 -0700

    snamilah

---
+++ exercise2.py
@@ -0,0 +1,7 @@
+import math
+radius_str = input ('ENTER THE RADIUS OF YOUR CIRCLE: ')
+radius_int = int (radius_str)
+circumference = 2 * math.pi * radius_int
+area = math.pi * (radius_int ** 2)
+print("THE CIRCUMFERENCES IS:",circumference, \
+	", AND THE AREA IS:",area)
