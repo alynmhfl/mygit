26432879eb9e98b1fd11a9bbf4dbf656d299aef0
maisya98 <shafiqah5012@gmail.com>
2020-11-02 01:47:11 -0800

    Week 3

--- 201102_example_1.py
+++ 201102_example_1.py
@@ -3,16 +3,16 @@ points_str = input("Enter the lead in points: ")
 points_remaining_int = int(points_str) 
 
 # 2. Subtract three. 
-lead_calculation_float= float(points_remaining_int - 3) 
+lead_calculation_float = float(points_remaining_int - 3) 
 
 # 3. Add a half—point if the team that is ahead has the ball, 
 # and subtract a half—point if the other team has the ball. 
 has_ball_str = input("Does the lead team have the ball (Yes or No): ") 
 
 if has_ball_str == 'Yes': 
-lead_calculation_float= lead_calculation_float + 0.5 
+	lead_calculation_float = lead_calculation_float + 0.5 
 else: 
-  lead_calculation_float= lead_calculation_float - 0.5 
+	lead_calculation_float = lead_calculation_float - 0.5 
 
 # (Numbers less than zero become zero) 
 if lead_calculation_float< 0: 
