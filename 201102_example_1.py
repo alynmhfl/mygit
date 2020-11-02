76beeaf4aa63160422f858a2e6550895d95ec8b3
Yong-Wang2 <Yong-Wang2@github.com>
2020-11-02 15:47:29 +0800

    any_comment

--- 201102_example_1.py
+++ 201102_example_1.py
@@ -10,13 +10,13 @@ lead_calculation_float= float(points_remaining_int - 3)
 has_ball_str = input("Does the lead team have the ball (Yes or No): ") 
 
 if has_ball_str == 'Yes': 
-lead_calculation_float= lead_calculation_float + 0.5 
+   lead_calculation_float= lead_calculation_float + 0.5 
 else: 
-  lead_calculation_float= lead_calculation_float - 0.5 
+   lead_calculation_float= lead_calculation_float - 0.5 
 
 # (Numbers less than zero become zero) 
 if lead_calculation_float< 0: 
-	lead_calculation_float= 0 
+        lead_calculation_float= 0 
 
 # 4. Square that. 
 lead_calculation_float= lead_calculation_float** 2 
