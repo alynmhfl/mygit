6661be43f8eddcd952c5ba1bbe362820d3643b6f
suhxx <nasuhanuar@gmail.com>
2020-11-03 04:14:35 +0800

    2/11/20

---
+++ 201102_example_3.py
@@ -0,0 +1,9 @@
+x_int = 0
+
+while x_int <10:
+    print(x_int, end='') #print the value each time through loop
+
+    x_int = x_int + 1 #update variable count for loop
+
+print()
+print("Final value of x_init:", x_int)
