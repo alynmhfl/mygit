8294c53690438f975ac227cdbb33f4736de14427
Cai Xuan <ccxuan123@gmail.com>
2020-11-02 15:14:54 +0800

    example 2

---
+++ 201102_example_2.py
@@ -0,0 +1,12 @@
+percent_float = float(input("What is your percent "))
+
+if 90 <= percent_float < 100:
+	print ("you received an A")
+elif 80 <= percent_float < 90:
+	print ("you received an B")
+elif 70 <= percent_float < 80:
+	print ("you received an C")
+elif 60 <= percent_float < 70:
+	print ("you received an D")
+else:
+	print("opps, not good")
