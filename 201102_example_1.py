6cbf0a8fb539382b5249296b38e4c5ff06e32441
dr-zool <dr-zool>
2020-11-01 19:58:43 -0800

    debuggin line 10

--- 201102_example_1.py
+++ 201102_example_1.py
@@ -10,9 +10,9 @@ lead_calculation_float= float(points_remaining_int - 3)
 has_ball_str = input("Does the lead team have the ball (Yes or No): ") 
 
 if has_ball_str == 'Yes': 
-lead_calculation_float= lead_calculation_float + 0.5 
+	lead_calculation_float= lead_calculation_float + 0.5 
 else: 
-  lead_calculation_float= lead_calculation_float - 0.5 
+	lead_calculation_float= lead_calculation_float - 0.5 
 
 # (Numbers less than zero become zero) 
 if lead_calculation_float< 0: 
@@ -24,7 +24,7 @@ lead_calculation_float= lead_calculation_float** 2
 seconds_remaining_int = int(input("Enter the number of seconds remaining: ")) 
 
 if lead_calculation_float> seconds_remaining_int: 
-  print("Lead is safe.") 
+	print("Lead is safe.") 
 else: 
-  print("Lead is not safe.") 
+	print("Lead is not safe.") 
 
