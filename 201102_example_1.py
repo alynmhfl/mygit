892b51d2be8282d58112676bc2d0da85c8d1cbd8
tsaqafi <73459739+tsaqafi@users.noreply.github.com>
2020-11-02 12:28:06 +0800

    change if else indent

--- 201102_example_1.py
+++ 201102_example_1.py
@@ -9,10 +9,10 @@ lead_calculation_float= float(points_remaining_int - 3)
 # and subtract a half—point if the other team has the ball. 
 has_ball_str = input("Does the lead team have the ball (Yes or No): ") 
 
-if has_ball_str == 'Yes': 
-lead_calculation_float= lead_calculation_float + 0.5 
-else: 
-  lead_calculation_float= lead_calculation_float - 0.5 
+if has_ball_str == 'Yes':
+	lead_calculation_float= lead_calculation_float + 0.5
+else:
+	lead_calculation_float= lead_calculation_float - 0.5
 
 # (Numbers less than zero become zero) 
 if lead_calculation_float< 0: 
