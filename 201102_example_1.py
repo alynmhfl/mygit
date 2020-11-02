b67595fbdc59a15f41566ae93af7910877107dd5
puiching-22 <puiching1997@graduate.utm.my>
2020-11-01 22:31:21 -0800

    Week 3

--- 201102_example_1.py
+++ 201102_example_1.py
@@ -10,9 +10,9 @@ lead_calculation_float= float(points_remaining_int - 3)
 has_ball_str = input("Does the lead team have the ball (Yes or No): ") 
 
 if has_ball_str == 'Yes': 
-lead_calculation_float= lead_calculation_float + 0.5 
+	lead_calculation_float= lead_calculation_float + 0.5 
 else: 
-  lead_calculation_float= lead_calculation_float - 0.5 
+	lead_calculation_float= lead_calculation_float - 0.5 
 
 # (Numbers less than zero become zero) 
 if lead_calculation_float< 0: 
@@ -20,11 +20,12 @@ if lead_calculation_float< 0:
 
 # 4. Square that. 
 lead_calculation_float= lead_calculation_float** 2 
+
 # 5. If the result is greater than the number of seconds left in the game, the lead is safe . 
 seconds_remaining_int = int(input("Enter the number of seconds remaining: ")) 
 
 if lead_calculation_float> seconds_remaining_int: 
-  print("Lead is safe.") 
+	print("Lead is safe.") 
 else: 
-  print("Lead is not safe.") 
+	print("Lead is not safe.") 
 
