515cc491a0e0a5c02160d16bd46e96cd3ca061f1
lhzulkifli <milesray717@yahoo.com>
2020-11-02 15:41:35 +0800

    if elif

---
+++ 201102_example_2.py
@@ -0,0 +1,12 @@
+percent_float = float(input('what is your percentage?'))
+
+if 90<=percent_float <100:
+    print ("you get an A")
+elif 80<=percent_float<90:
+    print('you get a B')
+elif 70<=percent_float>80:
+    print ('you get a C')
+elif 60<=percent_float>70:
+    print ('you get a D')
+else:
+    print ('oops, no good')
