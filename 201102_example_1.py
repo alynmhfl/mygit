348860a85caeff67fa766269c00f335a3a8cc70b
tsaqafi <73459739+tsaqafi@users.noreply.github.com>
2020-11-02 14:48:14 +0800

    Update 201102_example_1.py

--- 201102_example_1.py
+++ 201102_example_1.py
@@ -23,8 +23,7 @@ lead_calculation_float= lead_calculation_float** 2
 # 5. If the result is greater than the number of seconds left in the game, the lead is safe . 
 seconds_remaining_int = int(input("Enter the number of seconds remaining: ")) 
 
-if lead_calculation_float> seconds_remaining_int: 
-  print("Lead is safe.") 
-else: 
-  print("Lead is not safe.") 
-
+if lead_calculation_float> seconds_remaining_int:
+	print("Lead is safe.")
+else:
+	print("Lead is not safe.")
