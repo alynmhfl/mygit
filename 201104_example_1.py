986975214b8a6f9eb7ffe9b9d2d2beba64a52e39
Ubuntu <ubuntu@ubuntu1604.linuxvmimages.local>
2020-11-03 22:20:14 -0500

    Exercise 11/4/2020

---
+++ 201104_example_1.py
@@ -0,0 +1,22 @@
+#get an initial guess 
+guess_str = input("Guess a number: ") 
+guess = int(guess_str)  
+
+#initialise the value of number
+number = 65
+
+# while guess is range, keep asking 
+while 0 <= guess <= 100: 
+  if guess > number: 
+    print ("Guessed Too High.") 
+  elif guess < number: 
+    print("Guessed Too Low.") 
+  else: # correct guess, exit with break was: ",number) 
+    print("You guessed it. The number: ",number) 
+    break
+
+  # keep going, get the next guess 
+  guess_str = input("Guess a number: ") 
+  guess = int(guess_str) 
+else: 
+  print ("You quit early, the number was:",number) 
---
+++ 201104_example_4.py
@@ -0,0 +1,5 @@
+#install pandas (sudo apt install pyhton3-pandas)
+import pandas as pd
+year_index = { 'Year' :[y for y in range( 2010 , 2031 )],'Index' :[i for i in range( 21 )],}
+df = pd.DataFrame(year_index)
+print (df.head())
