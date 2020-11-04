1844bcd8588a4140736d2fe532f304de26b4cd60
Yong-Wang2 <Yong-Wang2@github.com>
2020-11-04 13:40:51 +0800

    any_comment

--- 201104_example_1.py
+++ 201104_example_1.py
@@ -1,17 +1,20 @@
 #get an initial guess 
-guess str = input("Guess a number: ") 
-guess = (guess_str)  
+import random
+number=random.randint(0, 100)
+guess_str = input("Guess a number: ") 
+guess = int(guess_str)  
  
 # while guess is range, keep asking 
 while 0 <= guess <= 100: 
-if guess > number: 
-print ("Guessed Too High.") 
-elif guess < number: 
-print("Guessed Too Low.") 
-else: # correct guess, exit with break was: ",number) 
-print("You guessed it. The number 
+	if guess > number: 
+		print ("Guessed Too High.") 
+	elif guess < number: 
+		print("Guessed Too Low.") 
+	else: # correct guess, exit with break was: ",number) 
+		print("You guessed it. The number is: ", number)
+		break
 # keep going, get the next guess 
-guess str = input("Guess a number: ") 
-guess = int(guess_str) 
+	guess_str = input("Guess a number: ") 
+	guess = int(guess_str) 
 else: 
-print ("You quit early, the number was:",number) 
+	print ("You quit early, the number was:",number) 
