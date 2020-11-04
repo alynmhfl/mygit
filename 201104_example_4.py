bbe226f272cc17c240259992dbc622bdccae7dc0
Chee-Kel-Syin <kelsyin@graduate.utm.my>
2020-11-04 12:09:57 +0800

    Week 3 -4/11/2020

--- 201104_example_4.py
+++ 201104_example_4.py
@@ -1,3 +1,5 @@
+# install the pandas module using the command sudo apt install python3-pip
+
 import pandas as pd
 year_index = { 'Year' :[y for y in range( 2010 , 2031 )],'Index' :[i for i in range( 21 )],}
 df = pd.DataFrame(year_index)
