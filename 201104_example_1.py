db0f284a653b17c270e5cce5603051747bda8af3
Ubuntu <ubuntu@ubuntu1604.linuxvmimages.local>
2020-11-03 22:25:15 -0500

    4112020Exercise

--- 201104_example_1.py
+++ 201104_example_1.py
@@ -1,17 +1,19 @@
 #get an initial guess 
-guess str = input("Guess a number: ") 
-guess = (guess_str)  
+number = 2;
+guess_str = input("Guess a number: ") 
+guess = int(guess_str)  
  
 # while guess is range, keep asking 
 while 0 <= guess <= 100: 
-if guess > number: 
-print ("Guessed Too High.") 
-elif guess < number: 
-print("Guessed Too Low.") 
-else: # correct guess, exit with break was: ",number) 
-print("You guessed it. The number 
+ if guess > number: 
+  print ("Guessed Too High.") 
+ elif guess < number: 
+  print("Guessed Too Low.") 
+ else: # correct guess, exit with break was: ",number) 
+  print("You guessed it. The number was:",number)
+  break
 # keep going, get the next guess 
-guess str = input("Guess a number: ") 
-guess = int(guess_str) 
+ guess_str = input("Guess a number: ") 
+ guess = int(guess_str) 
 else: 
-print ("You quit early, the number was:",number) 
+ print ("You quit early, the number was:",number) 
