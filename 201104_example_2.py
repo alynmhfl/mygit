03315d64664ffffe85e8b61791e00d17e806d886
izzuddin <izzuddin4hanif@gmail.com>
2020-11-04 12:58:08 +0800

    example 2 updated code

---
+++ 201104_example_2.py
@@ -0,0 +1,21 @@
+# initialize the input number and the sum
+
+str = input("Number: ")
+sum = 0
+
+# Stop if a period (.) is entered.
+# remember, number str is a string until we convert it
+while str != ".":
+    number = int(str)
+    if number % 2 == 1:  # number is not even (it is odd)
+        print("Error, only even numbers please.")
+        str = input("Number: ")
+        continue
+    sum += number
+    if sum != number:
+        break
+    else:
+        str = input("Number: ")
+print("Thes sum is: ",sum)
+
+
