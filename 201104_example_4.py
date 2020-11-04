01ef97cbfa8ab3c68f4490ab4d5b6cce9d6a0809
Ubuntu <ubuntu@ubuntu1604.linuxvmimages.local>
2020-11-03 22:31:11 -0500

    Example4 comment

--- 201104_example_4.py
+++ 201104_example_4.py
@@ -1,4 +1,7 @@
 import pandas as pd
+
 year_index = { 'Year' :[y for y in range( 2010 , 2031 )],'Index' :[i for i in range( 21 )],}
 df = pd.DataFrame(year_index)
+
+#print first five data
 print(df.head())
