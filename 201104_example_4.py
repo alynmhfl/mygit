12798f0b95ac8e949b8dcbaa2392d7184a3bb6b7
karoonlee <kolee1997@graduate.utm.my>
2020-11-04 17:03:10 -0800

    after edit

--- 201104_example_4.py
+++ 201104_example_4.py
@@ -2,4 +2,4 @@ import pandas as pd
 year_index = { 'Year' :[y for y in range( 2010 , 2031 )],'Index' :[i for i in range( 21 )],}
 df = pd.DataFrame(year_index)
 print(df.head())
-#the coding is working
+#this coding is working
