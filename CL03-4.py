c16146c1c77662948323b2bc1d375145356df87d
Chee-Kel-Syin <kelsyin@graduate.utm.my>
2020-11-09 17:08:02 +0800

    Week 4-9/11/2020

--- CL03-4.py
+++ CL03-4.py
@@ -1,12 +1,12 @@
 # read a particular line from a file. User provides both the line
 # number and the file name
 
-file_str = input ("Open what file: ")
+file_str = input("Open what file: ")
 find_line_str = input("Which line (integer):")
 
 try:
 	input_file = open(file_str)             #potential user error
-	file_line_int = int (find_line_str)     #potential user error
+	find_line_int = int(find_line_str)     #potential user error
 	line_count_int = 1
 	for line_str in input_file:
 		if line_count_int == find_line_int:
