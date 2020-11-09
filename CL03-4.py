ffa030d50e1f24a0bfceb342037fc87c151efe19
baaohuii <aaabaohui@gmail.com>
2020-11-09 15:06:04 +0800

    week 4

---
+++ CL03-4.py
@@ -0,0 +1,18 @@
+file_str = input("Open file:")
+find_line_str = input("Which line (integer):")
+
+try:
+	input_file = open(file_str)
+	find_line_int = int(find_line_str)
+	line_count_int = 1
+	for line_str in input_file:
+		if line_count_int == find_line_int:
+			print("Line {} of file {} is {}". format(find_line_int,file_str,line_str))
+			break
+		line_count_int+=1
+	else:
+		print("Line {} of file {} not found". format(find_line_int,file_str))		
+	input_file.close()
+
+except:
+	print("File {} not found".format(file_str))
