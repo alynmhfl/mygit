c9acca3b14b3546930523444edb59409784f72b3
xtrxllx <shazwanimizainiK15@gmail.com>
2020-11-08 20:45:27 -0800

    w3e1

---
+++ WEEK3/example3.py
@@ -0,0 +1,5 @@
+#Example with for-loop function
+diversity_list = [ 1 , 'One' , 1.0 , { '1' : 'One' }, { 'One' }]
+for element in diversity_list
+print(element)
+
---
+++ WEEK3/example4.py
@@ -0,0 +1,6 @@
+#Create an example with indexing statement
+import pandas as pd
+year_index = { 'Year' :[y for y in range( 2010 , 2031 )],'Index' :[i for i in range( 21 )],}
+df = pd.DataFrame(year_index)
+print(df.head())
+
