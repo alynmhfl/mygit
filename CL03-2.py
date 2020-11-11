10dd768cc662036ec4d3d0a13dea48a97594fa37
Yong-Wang2 <Yong-Wang2@github.com>
2020-11-11 14:51:27 +0800

    any_comment

---
+++ CL03-2.py
@@ -0,0 +1,9 @@
+#average of the sum integers in a given range
+limit_str =input("limit is 1 to input: ")
+limit_int =int(limit_str)
+count_int =1 
+sum_int =0
+while(count_int <= limit_int;
+	sum_int = sum_int + count_int
+	count_int =count_int +1
+average= float(sum_int)/(count_int-1)
