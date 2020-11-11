2a1b8f9f318933b9d0e8286f4bf0d2f647271186
maisya98 <shafiqah5012@gmail.com>
2020-11-10 18:32:44 -0800

    Exercise

---
+++ cal_and_plot.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+x = [0,1,2,3]
+y = [10,100,50,80]
+x2 = [0,1,2,3]
+y2 = [100,70,30,30]
+
+#define a function which can perform z = x/2 + y. then plot z.
+#name your file as cal_and_plot.py
+
+plt.bar(x,y, label = 'First Line')
+plt.bar(x2,y2, label = 'Second Line')
+plt.xlabel('Plot Number')
+plt.ylabel('Sample Value')
+plt.title('Sample Data\n 0 - 100')
+plt.legend()
+plt.show()
