5c59289397b348796a33c74d7f489226f8d4b6d9
Woods <kklimwei@gmail.com>
2020-11-16 15:08:46 +0800

    16th November

---
+++ CL06-5.py
@@ -0,0 +1,8 @@
+#Print ll words in a ditionart file that has one word per line
+
+#open file named "dictionary.txt" for reading ('r')
+data_file = open("dictionary.txt", 'r')
+
+#iterate through the file one line at a time
+for line_str in data_file:
+    print(line_str) 
