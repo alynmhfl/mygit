e7c825f978f4db8fc24851ab05a2a7ae276946b6
lhzulkifli <milesray717@yahoo.com>
2020-11-16 15:08:24 +0800

    16 nov lesson

--- aeiou_word_puzzle.py
+++ aeiou_word_puzzle.py
@@ -4,6 +4,7 @@ datafile=open('dictionary.txt','r')
 
 def clean_word(word):
     return word.strip().lower()
+    #return wordin lowercasestripped of whitespace
 
 def get_vowel(word):
     vowel_str="aeiou"
@@ -12,14 +13,15 @@ def get_vowel(word):
         if char in vowel_str:
             vowel_in_word+=char
     return vowel_in_word
+    #return vowels in string--includes repeats
 
 print('find words containing vowels aeiou in that order')
-for word in datafile:
-    word = clean_word(word)
-    if len(word)<=6:
+for word in datafile:              #for each word in file
+    word = clean_word(word)        #clean word
+    if len(word)<=6:               #if word is too small, skit it
         continue
-    vowel_str=get_vowel(word)
-    if vowel_str=='aeiou':
+    vowel_str=get_vowel(word)      #get vowels in word
+    if vowel_str=='aeiou':          #check if vowels are in this order
         print (word)
 
 
