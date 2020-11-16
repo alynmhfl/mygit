ab1e1b369a3a643c84f299e7846379e2e72379aa
heimrih <heimrih214@gmail.com>
2020-11-16 14:42:04 +0800

    Week 5 monday

---
+++ 201118_example_1.py
@@ -0,0 +1,14 @@
+def are_anagrams(word1,word2):
+  word1_sorted = sorted(word1)
+  word2_sorted = sorted(word2)
+  return word1_sorted == word2_sorted
+
+print("Anagram test")\
+
+two_words = input("Enter two space separated words: ")
+word1,word2 = two_words.split()
+
+if are_anagrams(word1,word2):
+  print("The words are anagrams.")
+else:
+  print("the words are not anagrams.")
