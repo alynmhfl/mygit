daf9ddf66b97a0a6f2a850e5067ac7c96cdecaec
Ubuntu <ubuntu@ubuntu1604.linuxvmimages.local>
2020-11-16 01:57:09 -0500

    Find_all_vowels

---
+++ find_vowels.py
@@ -0,0 +1,21 @@
+data_file=open("dictionary.txt",'r')
+
+def clean_word(word):
+    return word.strip().lower()
+
+def get_vowels_in_word(word):    
+    vowel_str="aeiou"
+    vowels_in_word=""
+    for char in word:
+        if char in vowel_str:
+            vowels_in_word += char
+    return vowels_in_word
+
+print("Find words containing vowels 'aeiou' in that order:")
+for word in data_file:
+    word=clean_word(word)
+    if len(word) <= 6:
+        continue
+    vowel_str=get_vowels_in_word(word)
+    if vowel_str=='aeiou':
+        print(word)
