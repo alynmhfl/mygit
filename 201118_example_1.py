0121a00e89d4a336858d14e53e683d01f6b717b8
heimrih <heimrih214@gmail.com>
2020-11-18 10:47:53 +0800

    Anagrams

--- 201118_example_1.py
+++ 201118_example_1.py
@@ -3,12 +3,21 @@ def are_anagrams(word1,word2):
   word2_sorted = sorted(word2)
   return word1_sorted == word2_sorted
 
-print("Anagram test")\
+print("Anagram test")
 
-two_words = input("Enter two space separated words: ")
-word1,word2 = two_words.split()
+valid_input_bool = False
+while not valid_input_bool:
+  try:
+    two_words = input("Enter two sapce separated words: ")
+    word1, word2 = two_words.split()
+
+    valid_input_bool = True
+  except ValueError:
+    print("Bad Input. Try again")
 
 if are_anagrams(word1,word2):
-  print("The words are anagrams.")
+  #print("The words are anagrams.")
+  print("the words {} and {} are anagrams.".format(word1,word2))
 else:
-  print("the words are not anagrams.")
+  #print("the words are not anagrams.")
+  print("the words {} and {} are not anagrams.".format(word1,word2))
