69420ed15fe3b140556aa511384ab995b03f5eb1
Cai Xuan <xuan1997@graduate.utm.my>
2020-11-18 11:27:59 +0800

    Week 5 example - Gettysburg address analysis

---
+++ 201118_example_2.py
@@ -0,0 +1,28 @@
+#Gettyburg address analysis
+#count words, unique words
+
+def make_word_list(a_file):
+    word_list=[]
+    #create a list of words from text file
+    for line_str in a_file:             #read line by line
+        line_list=line_str.split()      #split each line into list of words
+        for word in line_list:          #get words one line at a time from list
+            if word !="--":             #if word is not '--'
+                word_list.append(word)  #add word to speech list
+    return word_list
+
+#create a list of unique words
+def make_unique(word_list):    
+    unique_list=[]             
+
+    for word in word_list:
+        if word not in unique_list:    #if word is not already in unique list
+            unique_list.append(word)   #add to unique list
+    return unique_list
+
+gba_file=open('gettysburg.txt', 'r')
+speech_list=make_word_list(gba_file)
+
+print(speech_list)
+print('speech length: ', len(speech_list))
+print('unique length: ', len(make_unique(speech_list)))
